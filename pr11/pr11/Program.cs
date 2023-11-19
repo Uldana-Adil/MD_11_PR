5478e27c4abaa0ab5c6819b771730e2b2eab1706
Uldana-Adil <123613496+Uldana-Adil@users.noreply.github.com>
2023-11-19 23:39:37 +0600

    Add files via upload

---
+++ pr11/pr11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace pr11
+{
+    struct Employee 
+    {
+        public string Name;
+        public Vacancies Vacancy;
+        public int[] Date;
+        public int Salary;
+    
+    }
+    public struct Vacancies
+    {
+        public string Title;
+        public string Department;
+    }
+   
+}
---
+++ pr11/pr11/pr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr11
+{
+    class pr
+    {
+        static void Main()
+        {
+            Employee employee;
+
+            
+            employee.Name = "ADIL ULDANA";
+            employee.Vacancy.Title = "Software";
+            employee.Vacancy.Department = "IT";
+            employee.Date = new int[] { 2023, 11, 10 }; 
+            employee.Salary = 250000;
+
+            
+            Console.WriteLine($"Имя: {employee.Name}");
+            Console.WriteLine($"Вакансия: {employee.Vacancy.Title} в отделе {employee.Vacancy.Department}");
+            Console.WriteLine($"Дата приема на работу: {employee.Date[0]}.{employee.Date[1]}.{employee.Date[2]}");
+            Console.WriteLine($"Зарплата: {employee.Salary}");
+        }
+    }
+}
